507221152f0e8d58d8588af1bb33cacfb8a314e0
trieuntu <trieu.science@gmail.com>
2021-12-11 16:22:10 +0700

    distribution of the sentence lengths in FVnC dataset

---
+++ distribution_of_length.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pickle
+from pyvi import ViTokenizer, ViPosTagger
+import re
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'Latin Modern Math'
+from matplotlib import rc
+rc('text', usetex=True)
+
+def _save_pkl(path, obj):
+  with open(path, 'wb') as f:
+    pickle.dump(obj, f)
+def _load_pkl(path):
+  with open(path, 'rb') as f:
+    obj = pickle.load(f)
+  return obj 
+train=_load_pkl('../ntu_data/DLU_without_coincide_text_train.pkl')#DLU_without_coincide_text_train
+print(len(train))  
+seq_len = [len(i.split()) for i in train]
+ts=pd.Series(seq_len).hist(color="b", bins=300)
+ts.plot()
+ts.set_xlim((4,65))
+ts.grid(False)
+plt.show()
